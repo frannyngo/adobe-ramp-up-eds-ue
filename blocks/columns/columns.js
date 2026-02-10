f6efc620f788f8f2f0bd1ff93a727801a1ade579
Francis Tan <francist@ensemble.com>
2026-02-10 11:36:00 -0800

    header

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -5,7 +5,13 @@ export default function decorate(block) {
   const inner = block.firstElementChild;
   inner.className = "inner";
 
-  [...inner.children].forEach((child) => {
+  [...inner.children].forEach((child, index) => {
+    const isHeader = index === 0;
+
+    if (isHeader) {
+      child.className = "header-container";
+      return;
+    }
     console.log("*** child ", child);
   });
 }
