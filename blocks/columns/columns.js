c87f14bac21319b1a6019daef1b24f3eb3605331
Francis Tan <francist@ensemble.com>
2026-02-10 11:55:00 -0800

    more logs

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -19,7 +19,10 @@ export default function decorate(block) {
       if (i === 0) return; // title
       console.log("*** g ", g);
 
-      const placeholder = g.querySelector("p em")?.textContent;
+      const placeholder = g.querySelector("p em");
+      console.log("*** placeholder ", placeholder);
+      console.log("*** placeholder val ", placeholder.textContent);
+
       const input = document.createElement("input");
       input.placeholder = placeholder;
       input.required = true;
