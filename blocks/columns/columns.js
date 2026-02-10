0d34643eac113899b114b8605357d9b939980f05
Francis Tan <francist@ensemble.com>
2026-02-10 11:26:58 -0800

    inner css

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,8 +1,9 @@
 export default function decorate(block) {
   console.log("*** columns ", block);
+  block.className = "columns";
 
-  const inner = block.children.child;
-  inner.className = "columns";
+  const inner = block.firstElementChild;
+  inner.className = "inner";
 
   [...inner].forEach((child) => {
     console.log("*** child ", child);
